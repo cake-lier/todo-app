df887179a1977fd4bf196de31cd4d5c80f45e6e6
Elena Rughi <elena.rughi2@studio.unibo.it>
2022-01-20 10:29:07 +0100

    Fix socket.io import

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const path = require("path");
 global.appRoot = path.resolve(__dirname);
-require("mongoose").connect("mongodb://localhost:27017,localhost:27018,localhost:27019/todo?replicaSet=rs");
+require("mongoose").connect("mongodb://Eli-PC:27017,Eli-PC:27018,Eli-PC:27019/todo?replicaSet=rs");
 const app = express();
 app.use(express.json({ limit: 2097152 }));
 app.use("/static", express.static(__dirname + "/public"));
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const store = new MongoDBStore({
-    uri: "mongodb://localhost/todo",
+    uri: "mongodb://Eli-PC/todo",
     collection: "sessions"
 });
 app.use(session({
