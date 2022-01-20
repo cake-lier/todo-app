928a430b5b3c650790916cbf36543396ac1dd5fc
Elena Rughi <elena.rughi2@studio.unibo.it>
2022-01-20 19:51:29 +0100

    Add calendars(view only) for due date and reminder

--- client/src/components/list/List.js
+++ client/src/components/list/List.js
@@ -3,26 +3,40 @@ import { Checkbox } from 'primereact/checkbox';
 import { Tag } from 'primereact/tag';
 import { Button } from 'primereact/button';
 import { Menu } from 'primereact/menu';
+import { Calendar } from 'primereact/calendar';
+import { Dialog } from 'primereact/dialog';
 import "./List.scss";
 
 export function List(props) {
+    // checklist
     const listName = props.name;
-    const items = [{name: 'Take a picture', id: '00'},
-                    {name: 'Write report', id: '01'},
-                    {name: 'Production', id: '02'},
-                    {name: 'Research', id: '03'}];
+    const items = [{name: 'Take a picture', key: '00'},
+                    {name: 'Write report', key: '01'},
+                    {name: 'Production', key: '02'},
+                    {name: 'Research', key: '03'}];
     const [selectedItems, setSelectedItems] = useState(Array.prototype);
 
+    // task menu
     const menu = useRef(null);
     let menuItems = [
         {label: 'Edit', icon: 'pi pi-pencil'},
-        {label: 'Due date', icon: 'pi pi-calendar'},
+        {label: 'Due date', icon: 'pi pi-calendar', command:()=>{setDisplayCalendar1(true)}},
         {label: 'Assign to', icon: 'pi pi-user-plus' },
-        {label: 'Add reminder', icon: 'pi pi-bell'},
+        {label: 'Add reminder', icon: 'pi pi-bell', command:()=>{setDisplayCalendar2(true)}},
         {label: 'Add tags', icon: 'pi pi-tag' },
         {label: 'Add priority', icon: 'pi pi-star' },
         {label: 'Delete', icon: 'pi pi-trash'}
     ];
+
+    // calendar
+    const [displayCalendar1, setDisplayCalendar1] = useState(false);
+    const [displayCalendar2, setDisplayCalendar2] = useState(false);
+    const dialogFuncMap = {
+        'display1': setDisplayCalendar1,
+        'display2': setDisplayCalendar2
+    }
+    const [date1, setDate1] = useState(null);
+    const [date2, setDate2] = useState(null);
 
     // when checkbox is checked/unchecked, update selectedItems[]
     const onItemChange = (e) => {
@@ -35,13 +49,26 @@ export function List(props) {
             for (let i = 0; i < _selectedItems.length; i++) {
                 const selectedItem = _selectedItems[i];
 
-                if (selectedItem.id === e.value.id) {
+                if (selectedItem.key === e.value.key) {
                     _selectedItems.splice(i, 1);
                     break;
                 }
             }
         }
         setSelectedItems(_selectedItems);
+    }
+
+    // dialogs containing calendars
+    const calendarFooter = (btn_text, display) => {
+        return (
+            <div  className="flex justify-content-center">
+                <Button label={btn_text} onClick={() => onHide(display)} />
+            </div>
+        )
+    }
+
+    const onHide = (name) => {
+        dialogFuncMap[`${name}`](false);
     }
 
     return (
@@ -52,13 +79,13 @@ export function List(props) {
                     return (
                         <div className="flex justify-content-between m-2">
                             <div>
-                                <div id={item.id} className="field-checkbox m-1">
-                                    <Checkbox inputId={item.id}
+                                <div key={item.key} className="field-checkbox m-1">
+                                    <Checkbox inputId={item.key}
                                               name="item" value={item}
                                               onChange={onItemChange}
-                                              checked={selectedItems.some((i) => i.id === item.id)}
+                                              checked={selectedItems.some((i) => i.key === item.key)}
                                     />
-                                    <label htmlFor={item.id}>{item.name}</label>
+                                    <label htmlFor={item.key}>{item.name}</label>
                                 </div>
                                 <div className="flex align-items-center flex-wrap">
                                     <Tag className="flex m-1 p-tag-rounded" icon="pi pi-calendar">Jan 11</Tag>
@@ -69,10 +96,19 @@ export function List(props) {
                                     onClick={(e) => menu.current.toggle(e)}
                                     className="p-button-rounded p-button-icon-only p-button-text" />
                             <Menu model={menuItems} popup ref={menu} />
-                        </div>
+                            </div>
                     )
                 })
             }
+
+            <Dialog footer={calendarFooter('Set due date', 'display1')} dismissableMask={true} closable={false} visible={displayCalendar1} onHide={() => setDisplayCalendar1(false)}>
+                <Calendar id="time24" value={date1} onChange={(e) => setDate1(e.value)} inline />
+            </Dialog>
+
+            <Dialog footer={calendarFooter('Set reminder', 'display2')} dismissableMask={true} closable={false} visible={displayCalendar2} onHide={() => setDisplayCalendar2(false)}>
+                <Calendar id="time24" value={date2} onChange={(e) => setDate2(e.value)} showTime inline />
+            </Dialog>
+
         </div>
     )
 }
