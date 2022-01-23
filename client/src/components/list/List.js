5668ddce255bee573a4eef29143509e4cba4766b
Elena Rughi <elena.rughi2@studio.unibo.it>
2022-01-23 12:56:19 +0100

    Add new task dialog (frontend)

--- client/src/components/list/List.js
+++ client/src/components/list/List.js
@@ -1,6 +1,10 @@
+import React, { useState } from "react";
 import "./List.scss";
 import {Item} from "../Item";
 import { Button } from 'primereact/button';
+import {Dialog} from "primereact/dialog";
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
 
 export function List(props) {
     // checklist
@@ -11,11 +15,23 @@ export function List(props) {
                     {name: 'Research', key: '03'}];
 
     // create item
+    const [itemName, setItemName] = useState('');
+    const [itemNum, setItemNum] = useState(1);
     const onNewItem = () => {
-        console.log("new item");
+        setDisplayDialog(true);
+    }
+
+    const [displayDialog, setDisplayDialog] = useState(false);
+    const renderFooter = () => {
+        return (
+            <div  className="flex justify-content-center">
+                <Button label='Create' onClick={() => {setDisplayDialog(false)}}/>
+            </div>
+        )
     }
 
     return (
+        <>
         <div>
             <h2 className="font-medium text-3xl text-900">{listName}</h2>
             <Button label="New Task" icon="pi pi-plus" onClick={onNewItem}/>
@@ -25,5 +41,23 @@ export function List(props) {
                 })
             }
         </div>
+        <Dialog header='Create a new task' footer={renderFooter} dismissableMask={true} closable={false} visible={displayDialog} onHide={() => setDisplayDialog(false)}>
+            <label htmlFor="inputItemName">Name</label>
+            <span id="inputItemName" className="p-float-label">
+                <InputText id="itemName" value={itemName} onChange={(e) => setItemName(e.target.value)} />
+                <label htmlFor="itemName">Task Name</label>
+            </span>
+
+            <label htmlFor="num">Task number</label>
+            <div className="grid">
+                <div className="field col-12 md:col-3">
+                    <InputNumber
+                                 inputId="num" value={itemNum} onValueChange={(e) => setItemNum(e.value)}  showButtons buttonLayout="horizontal"
+                                 decrementButtonClassName="p-button-secondary" incrementButtonClassName="p-button-secondary" incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"
+                                 min={1} />
+                </div>
+            </div>
+        </Dialog>
+        </>
     )
 }
