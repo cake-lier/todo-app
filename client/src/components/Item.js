c22aeb4d9b7cf809221180c3886fa22980358ec5
Elena Rughi <elena.rughi2@studio.unibo.it>
2022-02-01 22:56:55 +0100

    Refactor item dialogs into separate files

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,11 +1,12 @@
+import React, {useRef, useState} from "react";
 import {Checkbox} from "primereact/checkbox";
 import {Tag} from "primereact/tag";
 import {Button} from "primereact/button";
 import {Menu} from "primereact/menu";
-import React, {useRef, useState} from "react";
-import {Dialog} from "primereact/dialog";
-import {Calendar} from "primereact/calendar";
 import {ItemCount} from "./ItemCount";
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+import {DueDateDialog} from "./itemDialogs/dueDateDialog";
+import {SetReminderDialog} from "./itemDialogs/setReminderDialog";
 
 export function Item({socket, item, onItemChange, selectedItems, deleteItem}){
     // item dot menu
@@ -23,72 +24,40 @@ export function Item({socket, item, onItemChange, selectedItems, deleteItem}){
     // calendar
     const [displayCalendar1, setDisplayCalendar1] = useState(false);
     const [displayCalendar2, setDisplayCalendar2] = useState(false);
-    const dialogFuncMap = {
-        'display1': setDisplayCalendar1,
-        'display2': setDisplayCalendar2
-    }
-    const [date1, setDate1] = useState(null);
-    const [date2, setDate2] = useState(null);
 
-    // dialogs containing calendars
-    const dueDateFooter = (btn_text, display) => {
-        return (
-            <div  className="flex justify-content-center">
-                <Button label={btn_text} onClick={() => {
-                    onHide(display);
-                    socket.emit('reminder', date2);
-                }} />
-            </div>
-        )
-    }
-
-    const reminderFooter = (btn_text, display) => {
-        return (
-            <div  className="flex justify-content-center">
-                <Button label={btn_text} onClick={() => {
-                    onHide(display);
-                    socket.emit('reminder', date2);
-                }} />
-            </div>
-        )
-    }
-
-    const onHide = (name) => {
-        dialogFuncMap[`${name}`](false);
-    }
+    // const itemTemplate = (list) => {
+    //     return (
+    //
+    //     );
+    // }
 
     return (
         <>
-        <div className="flex justify-content-between m-2">
-            <div>
-                <div className="field-checkbox m-1 mb-0">
-                    <Checkbox inputId={item._id}
-                              name="item" value={item}
-                              onChange={onItemChange}
-                              checked={selectedItems.some((i) => i._id === item._id)}
-                    />
-                    <label htmlFor={item._id}>{item.title}</label>
-                    <ItemCount maxCount={item.count} />
-                </div>
+            <div className="flex justify-content-between m-2">
+                <div>
+                    <div className="field-checkbox m-1 mb-0">
+                        <Checkbox inputId={item._id}
+                                  name="item" value={item}
+                                  onChange={onItemChange}
+                                  checked={selectedItems.some((i) => i._id === item._id)}
+                        />
+                        <label htmlFor={item._id}>{item.title}</label>
+                        <ItemCount maxCount={item.count} />
+                    </div>
 
-                <div className="flex align-items-center flex-wrap pl-4">
-                    <Tag className="flex m-1 p-tag-rounded" icon="pi pi-calendar">Jan 11</Tag>
-                    <Tag className="flex m-1 p-tag-rounded" icon="pi pi-circle-on">Unibo</Tag>
+                    <div className="flex align-items-center flex-wrap pl-4">
+                        <Tag className="flex m-1 p-tag-rounded" icon="pi pi-calendar">Jan 11</Tag>
+                        <Tag className="flex m-1 p-tag-rounded" icon="pi pi-circle-on">Unibo</Tag>
+                    </div>
                 </div>
+                <Button icon="pi pi-ellipsis-v"
+                        onClick={(e) => menu.current.toggle(e)}
+                        className="p-button-rounded p-button-icon-only p-button-text" />
+                <Menu model={menuItems} popup ref={menu} />
             </div>
-            <Button icon="pi pi-ellipsis-v"
-                    onClick={(e) => menu.current.toggle(e)}
-                    className="p-button-rounded p-button-icon-only p-button-text" />
-            <Menu model={menuItems} popup ref={menu} />
-        </div>
 
-        <Dialog footer={dueDateFooter('Set due date', 'display1')} dismissableMask={true} closable={false} visible={displayCalendar1} onHide={() => setDisplayCalendar1(false)}>
-            <Calendar id="time24" value={date1} onChange={(e) => setDate1(e.value)} inline stepMinute={2} />
-        </Dialog>
-
-        <Dialog footer={reminderFooter('Set reminder', 'display2')} dismissableMask={true} closable={false} visible={displayCalendar2} onHide={() => setDisplayCalendar2(false)}>
-            <Calendar id="time24" value={date2} onChange={(e) => setDate2(e.value)} showTime inline stepMinute={2} />
-        </Dialog>
+            <DueDateDialog itemId={item._id} displayCalendar={displayCalendar1} setDisplayCalendar={setDisplayCalendar1} />
+            <SetReminderDialog itemId={item._id} displayCalendar={displayCalendar2} setDisplayCalendar={setDisplayCalendar2} />
         </>
     )
 }
