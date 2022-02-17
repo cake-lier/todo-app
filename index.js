58ead8664e5c37bc8faa1bde0dd37d00ad4e753d
Matteo Castellucci <matteo.castellucci3@studio.unibo.it>
2022-02-18 00:14:34 +0100

    Fix bug which caused the blocking of reload after deploying the application

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 global.appRoot = path.resolve(__dirname);
-require("mongoose").connect("mongodb://localhost:27017,localhost:27018,localhost:27019/todo?replicaSet=rs");
+require("mongoose").connect(process.argv[2] ?? "mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/todo?replicaSet=rs");
 const app = express();
 app.use(express.json({ limit: 2097152 }));
 app.use("/static", express.static(__dirname + "/public"));
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 global.store = new MongoDBStore({
-    uri: "mongodb://localhost:27017,localhost:27018,localhost:27019/todo?replicaSet=rs",
+    uri: process.argv[2] ?? "mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/todo?replicaSet=rs",
     collection: "sessions"
 });
 app.use(session({
@@ -31,9 +31,6 @@ routes.initializeUserRoutes(app);
 routes.initializeListRoutes(app);
 routes.initializeItemRoutes(app);
 routes.initializeStaticRoutes(app);
-const { sendError, Error } = require("./src/utils/validation");
-app.use(
-    (_, response) => sendError(response, Error.ResourceNotFound)
-);
+app.get("*", (_, response) => response.sendFile(path.join(__dirname, "client/build/index.html")));
 require("./src/utils/schedule").scheduleTasks();
 global.io = require("./src/utils/sockets").setupSockets(app.listen(8080, () => console.log("Node API server started")));
