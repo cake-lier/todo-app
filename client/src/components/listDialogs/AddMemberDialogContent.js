43be385ee84d7959fe81292a9c8f94798a853fe6
Matteo Castellucci <matteo.castellucci3@studio.unibo.it>
2022-02-20 16:37:10 +0100

    Add more user friendly error for when inserted email is not correct

--- client/src/components/listDialogs/AddMemberDialogContent.js
+++ client/src/components/listDialogs/AddMemberDialogContent.js
@@ -5,6 +5,7 @@ import axios from "axios";
 
 export default function AddMemberDialogContent({ list, setDisplay, updateList, setMembers, displayError }){
     const [email, setEmail] = useState("");
+    const [emailErrorText, setEmailErrorText] = useState("");
     const joinListHandler = () => {
         axios.post(
             `/lists/${ list._id }/members`,
@@ -17,15 +18,19 @@ export default function AddMemberDialogContent({ list, setDisplay, updateList, s
             list => {
                 setDisplay(false);
                 axios.get(`/lists/${ list.data._id }/members/`)
-                     .then(
-                         members => setMembers(members.data),
-                         error => displayError(error.response.data.error)
-                     );
+                     .then(members => setMembers(members.data), error => displayError(error.response.data.error));
                 updateList(list.data);
             },
-            error => displayError(error.response.data.error)
+            error => {
+                if (error.response.data.error === 6) {
+                    setEmailErrorText("This email is not associated to any user.");
+                } else {
+                    displayError(error.response.data.error);
+                }
+            }
         );
     }
+    const isEmailFieldValid = () => emailErrorText === "";
     return (
         <div className="grid">
             <div className="col-12 m-0 flex flex-row align-items-center">
@@ -34,14 +39,15 @@ export default function AddMemberDialogContent({ list, setDisplay, updateList, s
                         <InputText
                             id="memberEmail"
                             name="memberEmail"
-                            className="w-full"
+                            className={ "w-full" + (isEmailFieldValid() ? "" : " p-invalid") }
                             value={ email }
                             onChange={ e => setEmail(e.target.value) }
                         />
-                        <label htmlFor="memberEmail">
+                        <label htmlFor="memberEmail" className={ isEmailFieldValid() ? "" : "p-error" }>
                             Member email
                         </label>
                     </span>
+                    { isEmailFieldValid() ? null : <p className="p-error mt-1 text-sm">{ emailErrorText }</p> }
                 </div>
                 <div className="col-3">
                     <Button
